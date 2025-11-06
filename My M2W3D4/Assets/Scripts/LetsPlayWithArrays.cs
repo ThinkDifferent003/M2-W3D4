2c8a0eaf30b4143474eac7bcdd223b18d6502bef
ThinkDifferent003 <rolandbibal3@gmail.com>
2025-11-06 10:29:14 +0100

    exx

--- My M2W3D4/Assets/Scripts/LetsPlayWithArrays.cs
+++ My M2W3D4/Assets/Scripts/LetsPlayWithArrays.cs
@@ -7,7 +7,42 @@ public class LetsPlayWithArrays : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int index = 0;
+        int times = 2;
+        int[] arrayNew = {1,2,3,4,5}; 
+        Debug.Log($"Valori iniziali");
+        Stampa(arrayNew);
+
         
+        //int value = Find(arrayNew,4);
+        //if (value != -1)
+        //{
+        //    Debug.Log($"TROVATO {value}");
+        //}
+       
+        
+        
+        //PutToTheBack(arrayNew, index);
+        //Debug.Log($"Valori dopo ");
+        //Stampa(arrayNew);
+
+
+        //ShiftOnce(arrayNew);
+        //Debug.Log($"Valori dopo");
+        //Stampa(arrayNew);
+
+
+        Shift(arrayNew, times);
+        Debug.Log($"Valori dopo");
+        Stampa(arrayNew);
+
+    }
+    void Stampa(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log($"Indice : {i} e valori {array[i]}");
+        }
     }
     int Find (int[] array,int value)
     {
@@ -15,26 +50,56 @@ public class LetsPlayWithArrays : MonoBehaviour
         {
            if (array[i] == value)
             {
-               return i;
+               
+                return i;
             }
+           
         }
-        return value;
+        return -1;
+        
+        
     }
     void PutToTheBack(int[] array, int index)
     {
-        int[] newarray = new int [5];
-        index = newarray[2];
-        for (int i = 0; i < 6 ;i++ )
+        index = array[2];
+
+        for (int i = 2 ; i < array.Length - 1; i++)
         {
-            if ( i == index)
-            {
-                index = array.Length + 1;
-            }
+            array[i] = array[i + 1];
+
+           
         }
+        array[array.Length - 1] = index;
+    }
+    void ShiftOnce(int[] array)
+    {
+        int last = array.Length;
+        for (int i = array.Length-1;i>0;i --)
+        {
+            array[i] = array[i-1];
+        }
+        array[0] = last;
+        
+    }
+    void Shift (int[] array , int times)
+    {
+        
+        int last = array.Length-1;
+        while(times > 0)
+        {
+            int i = array.Length - 1;
+            times--;
+            while (i > 0)
+            {
+                array[i] = array[i - 1];
+                array[0] = last;
+
+            }
+            
+        }
+        Debug.Log (array.Length);
 
     }
-
-
     // Update is called once per frame
     void Update()
     {
